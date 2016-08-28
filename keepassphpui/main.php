da4cd54cc06be7d8bd9af1507df47b0c844e3b4d
Louis Traynard <shkdee@users.noreply.github.com>
2016-08-28 17:33:09 +0200

    Guess MAX_FILE_SIZE from php.ini directives
    Try to guess MAX_FILE_SIZE so that it can be as high as possible in the
    current PHP configuration.

--- keepassphpui/main.php
+++ keepassphpui/main.php
@@ -6,10 +6,36 @@ define('KEEPASSPHP_LOCATION', 'keepassphp/keepassphp.php');
 // Debug mode for KeePassPHP
 define('KEEPASSPHP_DEBUG', false);
 
-// The maximum accepted size for uploaded files, in bytes. This is roughly 1 Mb
-// and should be okay for regular password databases. You may want to change
-// it if you expect specifically heavy databases.
-define("MAX_FILE_SIZE", 1048576);
+// The maximum accepted size for uploaded files, in bytes.
+// Try to guess it from the configuration directives upload_max_filesize and
+// post_max_size, but make it at least >= 1M in case our method fails.
+// That's already a quite big size for a password database.
+$post_max_size = parse_ini_size(ini_get('post_max_size'), 8388608);
+$upload_max_filesize = parse_ini_size(ini_get('upload_max_filesize'), 2097152);
+// If $post_max_size is 0, it should actually be ignored.
+define("MAX_FILE_SIZE", max(1048576, $post_max_size === 0
+	? $upload_max_filesize : min($post_max_size, $upload_max_filesize)));
+
+/**
+ * Parses the value of a configuration directive containing a size.
+ * Returns the result as an integer, defaulting to $default if the
+ * argument $ini_size is empty.
+ */
+function parse_ini_size($ini_size, $default)
+{
+	if(empty($ini_size) || $ini_size === false)
+		return $default;
+	switch(substr($ini_size, -1))
+	{
+		case 'M': case 'm': return (int)$ini_size * 1048576;
+		case 'K': case 'k': return (int)$ini_size * 1024;
+		case 'G': case 'g': return (int)$ini_size * 1073741824;
+		case 'B': case 'b': return parse_ini_size(substr($ini_size, 0, -1),
+			$default);
+		default: return intval($ini_size);
+	}
+}
+
 
 // We'll need this
 require_once "kphpui.php";
